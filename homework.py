1003ebe5dcce2554bf9bf27d1ffd789bb7a42085
Kirill Lerman <kirilllerman@gmail.com>
2021-10-17 20:04:57 +0800

    OOP HW, version 3, after 2 review

--- homework.py
+++ homework.py
@@ -2,7 +2,7 @@ import datetime as dt
 
 
 class Calculator:
-    TIME_DIFFERENCE = dt.timedelta(days=7)
+    TIME_DELTA = dt.timedelta(days=7)
 
     def __init__(self, limit):
         self.limit = limit
@@ -12,17 +12,19 @@ class Calculator:
         self.records.append(new_rec)
 
     def get_today_stats(self):
-        return sum(i.amount for i in self.records if i.date == dt.date.today())
+        tooday = dt.date.today()
+        return sum(record.amount for record in self.records if
+                   record.date == tooday)
 
     def get_week_stats(self):
         current = dt.date.today()
-        current_delta = current - Calculator.TIME_DIFFERENCE
-        return sum(i.amount for i in self.records
-                   if current_delta < i.date <= current)
+        current_delta = current - self.TIME_DELTA
+        return sum(record.amount for record in self.records
+                   if current_delta < record.date <= current)
 
 
 class Record:
-    CURRENT_DATE = '%d.%m.%Y'
+    DATE_TOODAY = '%d.%m.%Y'
 
     def __init__(self, amount, comment, date=None):
         self.amount = amount
@@ -30,24 +32,26 @@ class Record:
         if date is None:
             self.date = dt.date.today()
         else:
-            self.date = dt.datetime.strptime(date, Record.CURRENT_DATE).date()
+            self.date = dt.datetime.strptime(date, self.DATE_TOODAY).date()
 
 
 class CaloriesCalculator(Calculator):
-    STOP_PHRASE = 'Хватит есть!'
+    STOP = 'Хватит есть!'
+    ALLOWED = ('Сегодня можно съесть что-нибудь ещё, '
+               'но с общей калорийностью не более {number} кКал')
 
     def get_calories_remained(self):
         calories = self.limit - self.get_today_stats()
         if calories <= 0:
-            return CaloriesCalculator.STOP_PHRASE
-        return (f'Сегодня можно съесть что-нибудь ещё, '
-                f'но с общей калорийностью не более {calories} кКал')
+            return self.STOP
+        return self.ALLOWED.format(number=calories)
 
 
 class CashCalculator(Calculator):
-    STOP_PHRASE = 'Денег нет, держись'
-    ALOWED_PHRASE = ('На сегодня осталось {key1} {key2}')
-    DUTY_PHRASE = ('Денег нет, держись: твой долг - {key1} {key2}')
+    STOP = 'Денег нет, держись'
+    ALOWED = 'На сегодня осталось {number} {currency}'
+    DUTY = 'Денег нет, держись: твой долг - {number} {currency}'
+    ERROR = 'Ошибка. Значение валюты должно быть поддерживаемым'
     USD_RATE = 60.00
     EURO_RATE = 70.00
     RUB_RATE = 1.00
@@ -57,14 +61,14 @@ class CashCalculator(Calculator):
 
     def get_today_cash_remained(self, currency):
         cash = self.limit - self.get_today_stats()
+        if currency not in self.CURRUNCIES:
+            raise ValueError(self.ERROR)
         if cash == 0:
-            return CashCalculator.STOP_PHRASE
-        if currency not in CashCalculator.CURRUNCIES.keys():
-            raise ValueError('Введите значение rub, eur или usd')
-        cash_variable, rate = CashCalculator.CURRUNCIES[currency]
+            return self.STOP
+        cash_variable, rate = self.CURRUNCIES[currency]
         cash = round(cash / rate, 2)
         if cash > 0:
-            return CashCalculator.ALOWED_PHRASE.format(key1=cash,
-                                                       key2=cash_variable)
-        return CashCalculator.DUTY_PHRASE.format(key1=abs(cash),
-                                                 key2=cash_variable)
+            return self.ALOWED.format(number=cash,
+                                      currency=cash_variable)
+        return self.DUTY.format(number=abs(cash),
+                                currency=cash_variable)
